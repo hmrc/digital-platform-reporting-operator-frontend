69f6772e7e69b868c14506dfbbe84f1d21143791
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2024-10-03 11:30:11 +0100

    Correct postcode length for international addresses

--- app/forms/InternationalAddressFormProvider.scala
+++ app/forms/InternationalAddressFormProvider.scala
@@ -49,7 +49,7 @@ class InternationalAddressFormProvider @Inject() extends Mappings {
         ))),
       "postal" -> text("internationalAddress.error.postal.required")
         .verifying(firstError(
-          maxLength(25, "internationalAddress.error.postal.length"),
+          maxLength(10, "internationalAddress.error.postal.length"),
           regexp(Validation.textInputPattern.toString, "internationalAddress.error.postal.format")
         )),
       "country" -> text("internationalAddress.error.country.required")
