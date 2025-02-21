2c74ee393b42c635e5a07ef52b61068c37050eda
Hristo Georgiev <georgievh@yahoo.co.uk>
2025-02-21 16:24:24 +0000

    Libraries updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,18 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.8.0"
-  private val hmrcMongoVersion = "2.4.0"
+  private val bootstrapVersion = "9.9.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"    % "11.11.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "com.googlecode.libphonenumber"  % "libphonenumber"                % "8.13.54",
+    "com.googlecode.libphonenumber"  % "libphonenumber"                % "8.13.55",
     "com.beachape"                  %% "enumeratum-play"               % "1.8.2",
     "org.typelevel"                 %% "cats-core"                     % "2.13.0",
-    "uk.gov.hmrc"                   %% "crypto-json-play-30"           % "8.1.0",
+    "uk.gov.hmrc"                   %% "crypto-json-play-30"           % "8.2.0",
   )
 
   val test: Seq[ModuleID] = Seq(
