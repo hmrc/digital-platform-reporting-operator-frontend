75fe947c12d983c5af9415f35ca07e8bcdbbb45c
ShivangiShah42 <177633426+ShivangiShah42@users.noreply.github.com>
2026-04-09 11:25:48 +0100

    DLS-12797 : updated correlation id with requestID

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.6.0"
+  private val bootstrapVersion = "10.7.0"
   private val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -10,8 +10,8 @@ object AppDependencies {
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"    % "12.32.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "com.googlecode.libphonenumber"  % "libphonenumber"                % "9.0.1",
-    "com.beachape"                  %% "enumeratum-play"               % "1.8.2",
+    "com.googlecode.libphonenumber"  % "libphonenumber"                % "9.0.27",
+    "com.beachape"                  %% "enumeratum-play"               % "1.9.7",
     "org.typelevel"                 %% "cats-core"                     % "2.13.0",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"           % "8.4.0",
   )
@@ -20,7 +20,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.mockito"             %% "mockito-scala"           % "1.17.37"
+    "org.mockito"             %% "mockito-scala"           % "2.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
