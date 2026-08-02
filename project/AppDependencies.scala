bab578ffc3fdd0aa7568fce2f985c97f04638e54
ZaraFarzan <177644563+ZaraFarzan@users.noreply.github.com>
2026-08-02 16:17:02 +0100

    DLS-13280:[DPRS] Change pages with H1 elements using xl class to l across full service
    DLS-13280:[DPRS] Change pages with H1 elements using xl class to l across full service
    
    DLS-13280:[DPRS] Change pages with H1 elements using xl class to l across full service

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,15 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.12.0"
+  private val hmrcMongoVersion = "2.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"    % "13.9.0",
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "com.googlecode.libphonenumber"  % "libphonenumber"                % "9.0.31",
-    "com.beachape"                  %% "enumeratum-play"               % "1.9.7",
+    "com.googlecode.libphonenumber"  % "libphonenumber"                % "9.0.36",
+    "com.beachape"                  %% "enumeratum-play"               % "1.9.8",
     "org.typelevel"                 %% "cats-core"                     % "2.13.0",
     "uk.gov.hmrc"                   %% "crypto-json-play-30"           % "8.4.0",
   )
@@ -20,7 +20,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.18.0",
-    "org.mockito"             %% "mockito-scala"           % "2.2.1"
+    "org.mockito"             %% "mockito-scala"           % "2.2.3"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
